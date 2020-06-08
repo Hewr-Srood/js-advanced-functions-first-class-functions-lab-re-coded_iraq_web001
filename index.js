1f32fb5ae6aa6f2896806ff94beb278bd3697a3d
Hewr Srood <hewr_s@outlook.com>
2020-06-08 04:42:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers=(driverArr)=>[driverArr[0],driverArr[1]];
+const returnFirstTwoDrivers=(driverArr)=>[driverArr[driverArr.length-1],driverArr[driverArr.length-2]];
